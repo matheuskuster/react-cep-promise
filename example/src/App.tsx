87ad6dca0dfdb47f5c96b64fb2109a2669b5aee5
Matheus Kuster <matheuskuster@hotmail.com>
2020-07-29 18:30:44 -0300

    :tada: Initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-cep-promise'
+import 'react-cep-promise/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
