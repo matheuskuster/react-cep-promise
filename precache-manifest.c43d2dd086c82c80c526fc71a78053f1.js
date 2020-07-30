88a7946127478908de97835a702f71289cd480f4
Matheus Kuster <matheuskuster@hotmail.com>
2020-07-30 10:08:48 -0300

    Updates

---
+++ precache-manifest.c43d2dd086c82c80c526fc71a78053f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167200c9b675c80a110314ecebacf548",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5aff65924d579d3ec6e5",
+    "url": "./static/css/main.f9c8f25b.chunk.css"
+  },
+  {
+    "revision": "5f48c28e93856defd5c3",
+    "url": "./static/js/2.664f54b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.664f54b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aff65924d579d3ec6e5",
+    "url": "./static/js/main.f38840c7.chunk.js"
+  },
+  {
+    "revision": "280ba2964e192680626a",
+    "url": "./static/js/runtime-main.5a83e53b.js"
+  }
+]);
